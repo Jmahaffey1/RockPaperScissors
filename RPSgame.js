efd1030cfc27af8426b49abd4c87d3912f733f3d
Jacob Mahaffey <mahaffeyjake1@gmail.com>
2021-11-18 16:17:57 -0600

    Added wrong value catching and cleaned up functions

--- RPSgame.js
+++ RPSgame.js
@@ -1,6 +1,6 @@
 let possibleChoices = ['rock', 'paper', 'scissors'];
 
-function getRandInt(max){
+function getRandInt(max) {
     return Math.floor(Math.random() * max); // Used for generating computer's decision at random
 }
 
@@ -8,57 +8,65 @@ function computerPlay() {
     return possibleChoices[getRandInt(3)];
 }
 
-function playRound(playerSelection, computerSelection){
-    pSelection = playerSelection.toLowerCase();  //ensure lowercase value
-    cSelection = computerSelection.toLowerCase();
-
-    if(pSelection === cSelection){
-        console.log(pSelection + ' ' + cSelection);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log(playerSelection + ' ' + computerSelection);
         return ('It\'s a tie!')
-    } else if(pSelection === 'rock' && cSelection !== 'paper'){
-        console.log(pSelection + ' ' + cSelection);
+    } else if (playerSelection === 'rock' && computerSelection !== 'paper') {
+        console.log(playerSelection + ' ' + computerSelection);
         return ('Player Win');
-    } else if(pSelection === 'scissors' && cSelection !== 'rock') {
-        console.log(pSelection + ' ' + cSelection);
+    } else if (playerSelection === 'scissors' && computerSelection !== 'rock') {
+        console.log(playerSelection + ' ' + computerSelection);
         return ('Player Win');
-    } else if(pSelection === 'paper' && cSelection !== 'scissors'){
-        console.log(pSelection + ' ' + cSelection);
+    } else if (playerSelection === 'paper' && computerSelection !== 'scissors') {
+        console.log(playerSelection + ' ' + computerSelection);
         return ('Player Win');
     } else {
-        console.log(pSelection + ' ' + cSelection);
+        console.log(playerSelection + ' ' + computerSelection);
         return ('Computer Win');
     }
 
 }
 
-function game(){
-    let compWins = 0;
-    let humanWins = 0;
-    let ties = 0;
-    let rounds = Number(prompt('How many rounds would you like to play?'));
-    for(i=0;i<rounds;i++){
-        let plyrSelection = prompt('Take your pick!');
-        let compSelection = computerPlay();
-        let result = playRound(plyrSelection,compSelection);
-        if(result === 'Player Win'){
-            humanWins += 1;
-        } else if (result === 'It\'s a tie!'){
-            ties += 1;
-        } else {
-            compWins += 1;
-        }
-
-    }
-    if(ties > compWins && ties > humanWins){
-        console.log('The result was a tie: ');
+function determineWinner(compWins, humanWins, ties) {
+    if (ties > compWins && ties > humanWins) {
+        console.log('The match was a tie: ');
         console.log('Human Wins: ' + humanWins + ' Computer Wins: ' + compWins + ' Ties: ' + ties)
-    } else if (humanWins > compWins && humanWins > ties){
+    } else if (humanWins > compWins && humanWins > ties) {
         console.log('Human Wins!');
         console.log('Human Wins: ' + humanWins + ' Computer Wins: ' + compWins + ' Ties: ' + ties)
     } else if (compWins > humanWins && compWins > ties) {
         console.log('Computer Wins!')
         console.log('Human Wins: ' + humanWins + ' Computer Wins: ' + compWins + ' Ties: ' + ties)
     } else {
-        console.log('The match was inconclusive. The results were --- ' + 'Human Wins: ' + humanWins + ' Computer Wins: ' + compWins + ' Ties: ' + ties);
+        console.log('The match was inconclusive or exited. The results were --- ' + 'Human Wins: ' + humanWins + ' Computer Wins: ' + compWins + ' Ties: ' + ties);
     }
+}
+
+function game() {
+    let totalRounds = 0
+    let compWins = 0;
+    let humanWins = 0;
+    let ties = 0;
+    let rounds = Number(prompt('How many rounds would you like to play?'));
+    for (i = 0; i < rounds; i++) {
+        let plyrSelection = prompt('Take your pick!');
+        if (possibleChoices.includes(plyrSelection.toLowerCase())) {
+            let complayerSelection = computerPlay();
+            let result = playRound(plyrSelection, complayerSelection);
+            totalRounds += 1;
+            if (result === 'Player Win') {
+                humanWins += 1;
+            } else if (result === 'It\'s a tie!') {
+                ties += 1;
+            } else {
+                compWins += 1;
+            }
+
+        } else {
+            console.log('Illegal value entered. Game restarting');
+            game();
+        }
+    }
+    determineWinner(compWins, humanWins, ties);
 }
